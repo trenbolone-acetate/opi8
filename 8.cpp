00cb321414eee2ba5c7c601dcc18b0106a082018
cheez <cheezballz03@gmail.com>
2022-11-26 04:41:11 +0200

    mainbranch

---
+++ 8.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <iomanip> 
+#include "windows.h"
+#include "math.h"
+using namespace std;
+
+int main()
+{
+	//GitHub
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double b, h, y, x;
+	cout << "Введіть початкове значення x: ";
+	cin >> x;
+	cout << "Введіть кінцеве значення x: ";
+	cin >> b;
+	cout << "Вкажіть значення одного кроку: ";
+	cin >> h;
+	 do{
+		y = (cos(x) / sin(x)) + (2 * sin(x)) - 2;
+		cout << "x = " << x << "	y = " << setw(4) << y << endl;
+		x += h;
+	 } while (x > b);
+}
